6191760dcf8b4c7d00720361457ab058594cf249
Miguel de Barros <miguel@debarros.me>
2021-09-21 15:25:56 +0200

    chore: refactoring docs
    - moved docs folder to root
    - aligned vuepress plugins to other repos
    - updated main README to reflect the change
    - updated CI/CD scripts to publish to GH-pages
    - removed gitbooks files/references

---
+++ scripts/.publish-gh-pages.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+export GIT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
+echo "Publishing current branch: $GIT_BRANCH..."
+
+echo "Checking out gh-pages"
+git checkout gh-pages
+
+echo "Copying..."
+git pull mojaloop gh-pages --rebase
+
+echo "Install dependencies..."
+yarn
+
+echo "Build"
+yarn build
+
+echo "Staging general changes..."
+git add .
+
+echo "Staging distribution..."
+git add --force docs/.vuepress/dist
+
+echo "Staging versions..."
+git add --force website
+
+# commit
+git commit -a -m "Update gh-pages on github..."
+
+# push to the origin
+git push mojaloop gh-pages
